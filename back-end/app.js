06d661443964cc41cbb47316f9fef064f8ec5956
Kiyomi Rodgers Denniston <kiyomi.rodgersdenniston@gmail.com>
2022-02-11 10:51:22 -0500

    added middleware and route in app

--- back-end/app.js
+++ back-end/app.js
@@ -4,10 +4,17 @@ const express = require("express");
 
 // CONFIGURATION
 const app = express();
+const cors = require("cors");
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
+app.get("/", (req,res)=>{
+    res.send("<h1>Welcome to our Snack-A-Log</h1>");
+});
+app.get("/", )
 
 // EXPORT
 module.exports = app;
