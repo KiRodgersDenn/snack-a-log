e5f4b88201b84459efb55aa0fdc7ad4974b6f897
David Vidals <david.vidals08@gmail.com>
2022-02-11 11:56:48 -0500

    finished schema and app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 
 const express = require("express");
+const snacks = require('./controllers/snackController.js')
 
 // CONFIGURATION
 const app = express();
@@ -9,14 +10,16 @@ const cors = require("cors");
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use('snacks',snackController)
 
 // ROUTES
 app.get("/", (req,res)=>{
     res.send("<h1>Welcome to our Snack-A-Log</h1>");
 });
-app.get("/", )
+//catch all err message
+app.get("*",(req,res)=>{
+    req.status(404).send('page not found');
+} )
 
 // EXPORT
 module.exports = app;
-
-// Hello
